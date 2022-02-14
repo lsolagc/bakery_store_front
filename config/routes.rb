1dd920766704b8e1452e3da908d5826e1b815dc3
Lucas Sola <lsolagc@hotmail.com>
2022-02-14 19:49:19 -0300

    feat: adds Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :products
   resources :sizes
   resources :kinds
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
