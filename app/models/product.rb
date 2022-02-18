1b1092cbcee977f2abcecdaaaef8e35158822fe1
Lucas Sola <lsolagc@hotmail.com>
2022-02-17 21:07:41 -0300

    refactor: products have only one photo

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   has_many :kinds, through: :combinations
   has_many :sizes, through: :combinations
 
-  has_many_attached :photos
+  has_one_attached :photo
 
   accepts_nested_attributes_for :combinations, reject_if: :all_blank, allow_destroy: true
 
