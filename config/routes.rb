81bb88e0285853d71c18bdc1a6f80aee294a72f7
Lucas Sola <lsolagc@hotmail.com>
2022-02-19 15:22:07 -0300

    feat: adds Devise User

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :sizes
   resources :kinds
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
   ActiveAdmin.routes(self)
 
   root to: "welcome#index"
