d7593ef9e679eb3c69485bf6c8d1d2792476fe91
Lucas Sola <lsolagc@hotmail.com>
2022-02-28 13:51:10 -0300

    feat: makes combination optional, adds delegation

--- app/models/product_instance.rb
+++ app/models/product_instance.rb
@@ -1,4 +1,18 @@
 class ProductInstance < ApplicationRecord
-  belongs_to :combination
+  belongs_to :combination, required: false
   belongs_to :product
+
+  belongs_to :shopping_cart
+
+  delegate :combinations, :name, to: :product
+  delegate :price, to: :combination
+
+  before_save :update_total_price, if: -> { self.combination.present? }
+
+  # Callback methods
+
+  def update_total_price
+    total_price = self.quantity * self.price
+  end
+
 end
