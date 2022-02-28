c07524cafaa67ca2739c4ee7c6fd9f18d62310ea
Lucas Sola <lsolagc@hotmail.com>
2022-02-28 13:50:03 -0300

    feat: adds ShoppingCart and User admin views

---
+++ app/admin/shopping_carts.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register ShoppingCart do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :status, :total_value
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :status, :total_value]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+  permit_params :user_id, :status, :total_value
+
+end
