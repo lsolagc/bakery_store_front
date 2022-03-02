75cecf0a77dca0736714059d2e79c1f8a3c7a3c3
Lucas Sola <lsolagc@hotmail.com>
2022-03-02 10:40:33 -0300

    feat: adds shopping_cart edit view

---
+++ app/javascript/packs/product_instance.js
@@ -0,0 +1,18 @@
+$("[name='shopping_cart[product_instances][combination]']").on('change', function(){
+  row_node = this.closest('[data-product-instance]')
+  object_id = row_node.dataset["productInstance"]
+  $.ajax({
+    url: "/product_instances/"+ object_id +"/set_combination",
+    data: { combination_id: this.value },
+    dataType: 'json'
+  }).done(function(data) {
+    if(data != undefined) {
+      row_node.querySelector('input#shopping_cart_product_instances_total_value').value = data['total_value']
+      $('#shopping_cart_total_value').val(data['shopping_cart']['total_value'])
+    }
+    if(Object.keys(data.errors).length > 0){
+      console.log(data.errors)
+      // $('#product_instance-'+object_id+'-'+attribute+'-error').text(data.errors[attribute][0])
+    }
+  });
+})
--- app/models/combination.rb
+++ app/models/combination.rb
@@ -4,4 +4,10 @@ class Combination < ApplicationRecord
   belongs_to :size
 
   validates :price, presence: true
+
+  # Instance methods
+  def to_s
+    "#{self.kind.name} (#{self.size.name})"
+  end
+
 end
