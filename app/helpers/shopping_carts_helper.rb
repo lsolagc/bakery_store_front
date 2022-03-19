48e1c386c2420b704b06e95f5695bd597ff46a16
Lucas Sola <lsolagc@hotmail.com>
2022-03-19 20:47:41 -0300

    refactor: badge methods now return safe html

--- app/helpers/shopping_carts_helper.rb
+++ app/helpers/shopping_carts_helper.rb
@@ -1,12 +1,16 @@
 module ShoppingCartsHelper
   include ApplicationHelper
 
-  def status_badge_style(shopping_cart)
-    shopping_cart.canceled? ? 'badge bg-danger' : 'badge bg-info'
+  def status_badge(shopping_cart)
+    style = shopping_cart.canceled? ? 'badge bg-danger' : 'badge bg-info'
+    tag = "<span class='#{style}'>#{t("activerecord.attributes.shopping_cart.status.#{shopping_cart.status}")}</span>"
+    return tag.html_safe
   end
 
-  def payment_status_badge_style(shopping_cart)
-    shopping_cart.paid? ? 'badge bg-success' : 'badge bg-warning'
+  def payment_status_badge(shopping_cart)
+    style = shopping_cart.paid? ? 'badge bg-success' : 'badge bg-warning'
+    tag = "<span class='#{style}'>#{t("activerecord.attributes.shopping_cart.payment_status.#{shopping_cart.payment_status}")}</span>"
+    return tag.html_safe
   end
 
 end
