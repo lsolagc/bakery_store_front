685d3dec7344424c10e97c069beb3e2c12d57832
Lucas Sola <lsolagc@hotmail.com>
2022-03-23 20:13:51 -0300

    feat: add numericality and presence validation to rating score

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,8 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  validates :score,
+    numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 },
+    presence: true
+    
 end
